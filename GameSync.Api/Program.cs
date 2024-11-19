de9e510a707102617191890d0c9ba03533a1132f
DTroczyk <DTroczyk@gmail.com>
2024-11-19 20:46:47 +0100

    Add ASP.NET Identity configuration at Program.cs

--- GameSync.Api/Program.cs
+++ GameSync.Api/Program.cs
@@ -4,7 +4,10 @@ using System.Reflection;
 using FluentValidation;
 using GameSync.Api.Shared.Middleware;
 using GameSync.Application.Examples.Interfaces;
+using GameSync.Infrastructure.Context;
+using GameSync.Infrastructure.Context.Models;
 using GameSync.Infrastructure.Examples;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.OpenApi.Models;
 
 /// <summary>
@@ -27,23 +30,57 @@ public class Program
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
-        builder.Services.AddSwaggerGen(c =>
+        builder.Services.AddSwaggerGen(opt =>
         {
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "GameSync", Version = "v1" });
+            opt.SwaggerDoc("v1", new OpenApiInfo { Title = "GameSync", Version = "v1" });
+            opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = "bearer",
+            });
 
             var filePath = Path.Combine(AppContext.BaseDirectory, "GameSync.Api.xml");
-            c.IncludeXmlComments(filePath);
+            opt.IncludeXmlComments(filePath);
+
+            opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer",
+                    },
+                },
+                new string[] { }
+            },
+        });
         });
 
         var applicationAssembly = Assembly.Load("GameSync.Application");
         builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(applicationAssembly));
         builder.Services.AddValidatorsFromAssembly(applicationAssembly);
         builder.Services.AddDbContext<ExampleDbContext>();
+        builder.Services.AddDbContext<AppDbContext>();
         builder.Services.AddAutoMapper(applicationAssembly);
+
+        builder.Services.AddIdentityApiEndpoints<User>((options) =>
+        {
+            options.User.RequireUniqueEmail = false;
+            options.SignIn.RequireConfirmedEmail = false;
+        }).AddEntityFrameworkStores<AppDbContext>();
+        builder.Services.AddAuthorization();
 
         builder.Services.AddScoped<IExampleRepository, ExampleRepository>();
 
         var app = builder.Build();
+
+        app.MapGroup("/account").MapIdentityApi<User>();
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
