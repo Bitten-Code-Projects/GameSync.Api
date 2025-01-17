31e8896db763e19eae1c5e2c981510f494e7af4e
ogendogen <kosmos500@interia.pl>
2025-01-17 22:12:57 +0100

    Missing documentation

--- GameSync.Api/Middleware/RequestBodyLoggingMiddleware.cs
+++ GameSync.Api/Middleware/RequestBodyLoggingMiddleware.cs
@@ -4,17 +4,31 @@ using Microsoft.Extensions.Options;
 
 namespace GameSync.Api.Middleware;
 
+/// <summary>
+/// Request body logging middleware.
+/// </summary>
 public class RequestBodyLoggingMiddleware
 {
     private readonly RequestDelegate _next;
     private readonly ILogger _logger;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RequestBodyLoggingMiddleware"/> class.
+    /// </summary>
+    /// <param name="next">The request delegate.</param>
+    /// <param name="logger">The logger.</param>
     public RequestBodyLoggingMiddleware(RequestDelegate next, ILogger<RequestBodyLoggingMiddleware> logger)
     {
         _next = next;
         _logger = logger;
     }
 
+    /// <summary>
+    /// Invokation of middleware.
+    /// </summary>
+    /// <param name="context">The http context processed by middleware.</param>
+    /// <param name="options">The options snapshot. Lets configuring during runtime.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
     public async Task InvokeAsync(HttpContext context, IOptionsSnapshot<FeatureFlags> options)
     {
         if (!options.Value.EnableRequestLogging)
@@ -51,9 +65,16 @@ public class RequestBodyLoggingMiddleware
     }
 }
 
-// Extension method to make registration cleaner
+/// <summary>
+/// Extension method to make it easier to add the middleware.
+/// </summary>
 public static class RequestBodyLoggingMiddlewareExtensions
 {
+    /// <summary>
+    /// Response body middleware extension method.
+    /// </summary>
+    /// <param name="builder">The application builder.</param>
+    /// <returns>New application builder.</returns>
     public static IApplicationBuilder UseRequestBodyLogging(this IApplicationBuilder builder)
     {
         return builder.UseMiddleware<RequestBodyLoggingMiddleware>();
--- GameSync.Api/Middleware/ResponseBodyLoggingMiddleware.cs
+++ GameSync.Api/Middleware/ResponseBodyLoggingMiddleware.cs
@@ -3,17 +3,31 @@ using Microsoft.Extensions.Options;
 
 namespace GameSync.Api.Middleware;
 
+/// <summary>
+/// Response body logging middleware.
+/// </summary>
 public class ResponseBodyLoggingMiddleware
 {
     private readonly RequestDelegate _next;
     private readonly ILogger<ResponseBodyLoggingMiddleware> _logger;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ResponseBodyLoggingMiddleware"/> class.
+    /// </summary>
+    /// <param name="next">The request delegate.</param>
+    /// <param name="logger">The logger.</param>
     public ResponseBodyLoggingMiddleware(RequestDelegate next, ILogger<ResponseBodyLoggingMiddleware> logger)
     {
         _next = next;
         _logger = logger;
     }
 
+    /// <summary>
+    /// Invokation of middleware.
+    /// </summary>
+    /// <param name="context">The http context processed by middleware.</param>
+    /// <param name="options">The options snapshot. Lets configuring during runtime.</param>
+    /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
     public async Task Invoke(HttpContext context, IOptionsSnapshot<FeatureFlags> options)
     {
         if (!options.Value.EnableRequestLogging)
@@ -62,9 +76,16 @@ public class ResponseBodyLoggingMiddleware
     }
 }
 
-// Extension method to make it easier to add the middleware
+/// <summary>
+/// Extension method to make it easier to add the middleware.
+/// </summary>
 public static class ResponseBodyMiddlewareExtensions
 {
+    /// <summary>
+    /// Response body middleware extension method.
+    /// </summary>
+    /// <param name="builder">The application builder.</param>
+    /// <returns>New application builder.</returns>
     public static IApplicationBuilder UseResponseBodyMiddleware(this IApplicationBuilder builder)
     {
         return builder.UseMiddleware<ResponseBodyLoggingMiddleware>();
