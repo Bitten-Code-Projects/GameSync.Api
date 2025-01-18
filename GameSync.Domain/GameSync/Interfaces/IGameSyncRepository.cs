d582dd85165e4d9a0c7341bcd23b4a2d61ba1616
mkosmala <mkosmala@pgs-soft.com>
2025-01-18 17:15:09 +0100

    Remove old code

--- GameSync.Domain/GameSync/Interfaces/IGameSyncRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using GameSync.Domain.GameSync.Entities;
-
-namespace GameSync.Domain.GameSync.Interfaces;
-
-public interface IGameSyncRepository
-{
-    Task<LogEntity> CreateLogEntityAsync(LogEntity logEntity, CancellationToken cancellationToken = default);
-}
